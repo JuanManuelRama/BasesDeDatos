eaafb97e054ea7eca2cb4db007aaa0736cb82d60
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-06-24 18:02:09 -0300

    Cambios (no me pagan por mensajes, mañana hablamos)

--- data/script_creación_BI.sql
+++ data/script_creación_BI.sql
@@ -2,26 +2,36 @@ USE GD1C2025
 GO
 
 DROP TABLE IF EXISTS BI_Fact_Table_Factura
+DROP TABLE IF EXISTS BI_Fact_Table_Compra
 DROP TABLE IF EXISTS BI_Sucursal
 DROP TABLE IF EXISTS BI_Fecha
 DROP TABLE IF EXISTS BI_Cliente
-DROP TABLE IF EXISTS BI_Ubicacion
-DROP TABLE IF EXISTS BI_Provincia
 DROP TABLE IF EXISTS BI_Modelo
 DROP TABLE IF EXISTS BI_Factura
+DROP TABLE IF EXISTS BI_Material
 DROP VIEW IF EXISTS Factura_Promedio_Mensual
 DROP VIEW IF EXISTS Rendimiento_de_modelos
+DROP VIEW IF EXISTS Promedio_De_Compras
+DROP VIEW IF EXISTS Compra_Por_Tipo_De_Material
+DROP VIEW IF EXISTS Ganancias
 GO
 
 
 CREATE TABLE BI_Fecha (
 	fecha_id BIGINT IDENTITY(1,1),
-	fecha_a�o INT,
+	fecha_año INT,
 	fecha_mes INT,
-	fecha_cuatrimestre INT,
+	fecha_cuatrimestre AS 
+	CASE 
+        WHEN fecha_mes BETWEEN 1 AND 3 THEN 1
+        WHEN fecha_mes BETWEEN 4 AND 6 THEN 2
+        WHEN fecha_mes BETWEEN 7 AND 9 THEN 3
+        ELSE 4
+    END 
+	PERSISTED,
 	CONSTRAINT PK_Fecha PRIMARY KEY (fecha_id),
-	CONSTRAINT UQ_Fecha UNIQUE (fecha_a�o, fecha_mes),
-	CONSTRAINT CK_Fecha_Cuatrimestre CHECK (fecha_cuatrimestre IN(1, 2, 3, 4))
+	CONSTRAINT UQ_Fecha UNIQUE (fecha_año, fecha_mes),
+	CONSTRAINT CK_Fecha_Mes CHECK (fecha_mes BETWEEN 1 AND 12)
 )
 
 CREATE TABLE BI_Cliente (
@@ -33,27 +43,11 @@ CREATE TABLE BI_Cliente (
 	CONSTRAINT UQ_Cliente UNIQUE (cliente_minimo, cliente_maximo, cliente_rango)
 )
 
-CREATE TABLE BI_Provincia (
-	provincia_id BIGINT IDENTITY(1,1),
-	provincia_nombre VARCHAR(50),
-	CONSTRAINT PK_Provincia PRIMARY KEY (provincia_id),
-	CONSTRAINT UQ_Provincia UNIQUE (provincia_nombre)
-)
-
-CREATE TABLE BI_Ubicacion (
-	ubicacion_id BIGINT IDENTITY(1,1),
-	ubicacion_localidad VARCHAR(50),
-	ubicacion_provincia BIGINT,
-	CONSTRAINT PK_Ubicacion PRIMARY KEY (ubicacion_id),
-	CONSTRAINT FK_Ubicacion_Provincia FOREIGN KEY (ubicacion_provincia) REFERENCES BI_Provincia,
-	CONSTRAINT UQ_Ubicacion UNIQUE (ubicacion_provincia, ubicacion_localidad)
-)
-
 CREATE TABLE BI_Sucursal (
 	sucursal_id BIGINT,
-	sucursal_ubicacion BIGINT
+	sucursal_provincia VARCHAR(50),
+	sucursal_localidad VARCHAR(50),
 	CONSTRAINT PK_Sucursal PRIMARY KEY (sucursal_id),
-	CONSTRAINT FK_Sucursal_Ubicacion FOREIGN KEY (sucursal_ubicacion) REFERENCES BI_Ubicacion
 )
 
 CREATE TABLE BI_Modelo (
@@ -67,23 +61,42 @@ CREATE TABLE BI_Factura (
 	CONSTRAINT PK_Factura PRIMARY KEY (factura_id)
 )
 
+CREATE TABLE BI_Material (
+	material_id  INT IDENTITY(1,1),
+	material_nombre varchar(50),
+	CONSTRAINT PK_Material PRIMARY KEY (material_id),
+	CONSTRAINT UQ_Material UNIQUE (material_nombre)
+)
+
 CREATE TABLE BI_Fact_Table_Factura (
 	id_fecha BIGINT,
 	id_cliente BIGINT,
 	id_sucursal BIGINT,
-	--id_ubicacion BIGINT,
 	id_modelo BIGINT,
 	id_factura BIGINT,
 	fact_total DECIMAl(12,2),
-	--CONSTRAINT PK_Fact_Table_Factura PRIMARY KEY (id_fecha, fact_total),
-	CONSTRAINT FK_Fact_Table_Factura_Fecha FOREIGN KEY (id_fecha) REFERENCES BI_fecha,
+	CONSTRAINT FK_Fact_Table_Factura_Fecha FOREIGN KEY (id_fecha) REFERENCES BI_Fecha,
+	CONSTRAINT FK_Fact_Table_Factura_Cliente FOREIGN KEY (id_cliente) REFERENCES BI_Cliente,
+	CONSTRAINT FK_Fact_Table_Factura_Sucursal FOREIGN KEY (id_sucursal) REFERENCES BI_Sucursal,
 	CONSTRAINT FK_Fact_Table_Factura_Modelo FOREIGN KEY (id_modelo) REFERENCES BI_Modelo,
 	CONSTRAINT FK_Fact_Table_Factura_Factura FOREIGN KEY (id_factura) REFERENCES BI_Factura
+)
+
+CREATE TABLE BI_Fact_Table_Compra (
+	id_fecha BIGINT,
+	id_sucursal BIGINT,
+	id_material INT,
+	compra_total DECIMAL(12,2),
+	--CONSTRAINT PKFact_Table_Compra PRIMARY KEY (id_fecha, id_sucursal, id_material)
+	CONSTRAINT FK_Fact_Table_Compra_Fecha FOREIGN KEY (id_fecha) REFERENCES BI_Fecha,
+	CONSTRAINT FK_Fact_Table_Compra_Sucursal FOREIGN KEY (id_sucursal) REFERENCES BI_Sucursal,
+	CONSTRAINT FK_Fact_Table_Compra_Material FOREIGN KEY (id_material) REFERENCES BI_Material
+
 )
 GO
 
 INSERT INTO BI_Fecha (
-	fecha_a�o,
+	fecha_año,
 	fecha_mes
 )
 SELECT DISTINCT YEAR(Factura_Fecha), 
@@ -106,13 +119,6 @@ SELECT DISTINCT YEAR(Compra_Fecha),
 			    MONTH(Compra_Fecha)
 FROM DROP_DATABASE.Compra
 
-UPDATE BI_Fecha
-SET fecha_cuatrimestre = CASE
-    WHEN fecha_mes BETWEEN 1 AND 4 THEN 1
-    WHEN fecha_mes BETWEEN 5 AND 8 THEN 2
-    ELSE 3
-END
-
 INSERT INTO BI_CLIENTE (
 	cliente_minimo,
 	cliente_maximo,
@@ -124,35 +130,19 @@ VALUES (0, 24, '<24'),
 	   (50, 100000, '>50')
 
 
-INSERT INTO BI_Provincia (
-	provincia_nombre
-)
-SELECT Provincia_Nombre
-FROM DROP_DATABASE.Provincia
-
-INSERT INTO BI_Ubicacion (
-	ubicacion_provincia,
-	ubicacion_localidad
-)
-SELECT BI_Provincia.provincia_id, Localidad_Nombre
-FROM DROP_DATABASE.Localidad
-JOIN BI_Provincia ON Provincia_ID = Localidad_Provincia
-
-
 INSERT INTO BI_Sucursal (
     sucursal_id,
-    sucursal_ubicacion
+    sucursal_provincia,
+	sucursal_localidad
 )
 SELECT 
     Sucursal_Numero,
-    ubicacion_id
+    Provincia_Nombre,
+	Localidad_Nombre
 FROM DROP_DATABASE.Sucursal
 JOIN DROP_DATABASE.Domicilio ON Domicilio_ID = Sucursal_Domicilio
 JOIN DROP_DATABASE.Localidad ON Localidad_ID = Domicilio_Localidad
 JOIN DROP_DATABASE.Provincia p ON Provincia_ID = Localidad_Provincia
-JOIN BI_Provincia bp ON bp.provincia_nombre = p.Provincia_Nombre
-JOIN BI_Ubicacion u ON ubicacion_localidad = Localidad_Nombre
-					AND ubicacion_provincia = bp.provincia_id
 
 INSERT INTO BI_Modelo (
 	modelo_id,
@@ -168,6 +158,14 @@ INSERT INTO BI_Factura (
 SELECT Factura_Numero
 FROM DROP_DATABASE.Factura
 
+INSERT INTO BI_Material (
+	material_nombre
+)
+SELECT DISTINCT Material_Tipo
+FROM DROP_DATABASE.Material
+
+GO
+--Tablas de hechos
 INSERT INTO BI_Fact_Table_Factura (
 	id_fecha,
 	id_cliente,
@@ -185,57 +183,109 @@ SELECT fecha_id,
 FROM DROP_DATABASE.Factura
 JOIN DROP_DATABASE.Detalle_Factura ON Detalle_Factura_Factura = Factura_Numero
 JOIN DROP_DATABASE.Cliente ON Cliente_ID = Factura_Cliente
-JOIN BI_Fecha ON fecha_a�o = YEAR(Factura_Fecha)
+JOIN BI_Fecha ON fecha_año = YEAR(Factura_Fecha)
 			  AND fecha_mes = MONTH(Factura_Fecha)
 JOIN BI_Cliente ON DATEDIFF(YEAR, Cliente_Fecha_Nacimiento, GETDATE()) BETWEEN cliente_minimo AND cliente_maximo
 JOIN BI_Sucursal ON BI_Sucursal.sucursal_id = Factura_Sucursal
 JOIN DROP_DATABASE.Sillon ON Detalle_Factura_Detalle_Pedido = Sillon_Codigo
+
+INSERT INTO BI_Fact_Table_Compra (
+	id_fecha,
+	id_sucursal,
+	id_material,
+	compra_total
+)
+SELECT fecha_id,
+       Compra_Sucursal,
+	   mb.material_id,
+	   Detalle_Compra_Subtotal
+FROM DROP_DATABASE.Compra
+JOIN DROP_DATABASE.Detalle_Compra ON Detalle_Compra_Compra = Compra_Numero
+JOIN DROP_DATABASE.Material m ON Material_ID = Detalle_Compra_Material
+JOIN BI_Fecha ON fecha_año = YEAR(Compra_Fecha)
+			  AND fecha_mes = MONTH(Compra_Fecha)
+JOIN BI_Material mb ON mb.material_nombre = m.Material_Tipo
 GO
 --Vistas
+CREATE VIEW Ganancias
+AS
+	SELECT  fecha_año,
+        	fecha_mes,
+        	sucursal_id,
+       		ISNULL(SUM(fact_total),0) - ISNULL(SUM(compra_total),0) AS Ganancias
+	FROM BI_Fecha
+	CROSS JOIN BI_Sucursal
+	LEFT JOIN BI_Fact_Table_Factura ON id_fecha = fecha_id
+									AND id_sucursal = sucursal_id
+	LEFT JOIN BI_Fact_Table_Compra c ON c.id_fecha = fecha_id
+								   AND c.id_sucursal  = sucursal_id
+	GROUP BY fecha_año,
+			 fecha_mes,
+			 sucursal_id
+GO
+
 CREATE VIEW Factura_Promedio_Mensual
 AS
-SELECT fecha_a�o,
-	   fecha_cuatrimestre,
-	   provincia_nombre,
-	   SUM(fact_total) / COUNT(DISTINCT id_factura) AS Promedio_Facturado
-FROM BI_Fact_Table_Factura
-JOIN BI_Fecha ON fecha_id = id_fecha
-JOIN BI_Sucursal ON sucursal_id = id_sucursal
-JOIN BI_Ubicacion ON ubicacion_id = sucursal_ubicacion
-JOIN BI_Provincia ON provincia_id = ubicacion_provincia
-GROUP BY fecha_a�o, 
-         fecha_cuatrimestre, 
-		 provincia_nombre
+	SELECT fecha_año,
+		fecha_cuatrimestre,
+		sucursal_provincia,
+		SUM(fact_total) / COUNT(DISTINCT id_factura) AS Promedio_Facturado
+	FROM BI_Fact_Table_Factura
+	JOIN BI_Fecha ON fecha_id = id_fecha
+	JOIN BI_Sucursal ON sucursal_id = id_sucursal
+	GROUP BY fecha_año, 
+			fecha_cuatrimestre, 
+			sucursal_provincia
 GO
 CREATE VIEW Rendimiento_de_modelos
 AS
-SELECT modelo_nombre, 
-	   fecha_a�o,
-	   fecha_cuatrimestre,
-	   ubicacion_localidad,
-	   cliente_rango
-FROM BI_Fact_Table_Factura
-JOIN BI_Modelo m ON modelo_id = id_modelo
-JOIN BI_Fecha f ON fecha_id = id_fecha
-JOIN BI_Sucursal s ON sucursal_id = id_sucursal
-JOIN BI_Ubicacion u ON ubicacion_id = sucursal_ubicacion
-JOIN BI_Cliente c ON cliente_id = id_cliente
-GROUP BY modelo_nombre,
-		 fecha_a�o,
-		 fecha_cuatrimestre,
-		 ubicacion_localidad,
-		 cliente_rango,
-		 modelo_id
-HAVING modelo_id IN (SELECT TOP 3 ft2.id_modelo
-					 FROM BI_Fact_Table_Factura ft2
-					 JOIN BI_Fecha ON ft2.id_fecha = fecha_id
-					 JOIN BI_Sucursal ON id_sucursal = sucursal_id
-					 JOIN BI_Ubicacion u2 ON sucursal_ubicacion = u2.ubicacion_id
-					 JOIN BI_Cliente c2       ON ft2.id_cliente = c2.cliente_id
-					 WHERE fecha_a�o = f.fecha_a�o
-						AND fecha_cuatrimestre = f.fecha_cuatrimestre
-						AND ubicacion_localidad = u.ubicacion_localidad
-						AND cliente_rango = c.cliente_rango
-				    GROUP BY ft2.id_modelo
-					ORDER BY SUM(ft2.fact_total) DESC)
+	SELECT modelo_nombre, 
+		   fecha_año,
+		   fecha_cuatrimestre,
+		   sucursal_localidad,
+		   cliente_rango
+	FROM BI_Fact_Table_Factura
+	JOIN BI_Modelo m ON modelo_id = id_modelo
+	JOIN BI_Fecha f ON fecha_id = id_fecha
+	JOIN BI_Sucursal s ON sucursal_id = id_sucursal
+	JOIN BI_Cliente c ON cliente_id = id_cliente
+	GROUP BY modelo_nombre,
+			fecha_año,
+			fecha_cuatrimestre,
+			sucursal_localidad,
+			cliente_rango,
+			modelo_id
+	HAVING modelo_id IN (SELECT TOP 3 id_modelo
+						 FROM BI_Fact_Table_Factura
+						 JOIN BI_Fecha ON id_fecha = fecha_id
+						 JOIN BI_Sucursal ON id_sucursal = sucursal_id
+						 JOIN BI_Cliente ON id_cliente = cliente_id
+						 WHERE fecha_año = f.fecha_año
+							AND fecha_cuatrimestre = f.fecha_cuatrimestre
+							AND sucursal_localidad = s.sucursal_localidad
+							AND cliente_rango = c.cliente_rango
+						 GROUP BY id_modelo
+						 ORDER BY SUM(fact_total) DESC)
+GO
+CREATE VIEW Promedio_De_Compras
+AS
+	SELECT AVG(compra_total) AS promedio_compra_mensual,
+		   fecha_mes
+	FROM BI_Fact_Table_Compra
+	JOIN BI_Fecha ON fecha_id = id_fecha
+	GROUP BY fecha_mes
+GO
+CREATE VIEW Compra_Por_Tipo_De_Material
+AS
+	SELECT SUM(compra_total) AS importe_total_gastado,
+		   material_nombre,
+		   sucursal_id,
+		   fecha_cuatrimestre
+	FROM BI_Fact_Table_Compra
+	JOIN BI_Material ON material_id = id_material
+	JOIN BI_Fecha ON fecha_id = id_fecha
+	JOIN BI_Sucursal ON sucursal_id = id_sucursal
+	GROUP BY material_nombre,
+			 sucursal_id,
+			 fecha_cuatrimestre
 GO
