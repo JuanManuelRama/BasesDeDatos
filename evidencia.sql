6b696abcef0c6c406f3b4994932bbccd690ebb33
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-06-26 12:15:01 -0300

    Borro archivos innecesarios

--- evidencia.sql
+++
@@ -1,4 +0,0 @@
-SELECT Pedido_Numero, Detalle_Pedido_SubTotal, Sillon_Codigo, Factura_Numero, Detalle_Factura_SubTotal
-FROM gd_esquema.Maestra
-WHERE Pedido_Numero IS NOT NULL
-ORDER BY Pedido_Numero, Detalle_Pedido_SubTotal
